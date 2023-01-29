e71d0a7f010c1fd117cacf9e41797080cf42d2eb
ducknowledges <ducknowledges@gmail.com>
2023-01-29 17:30:50 +0500

    refactor[hw02-containers]: change container to array deque

--- hw02-containers/src/main/java/com/github/ducknowledges/containers/CustomerReverseOrder.java
+++ hw02-containers/src/main/java/com/github/ducknowledges/containers/CustomerReverseOrder.java
@@ -1,17 +1,18 @@
 package com.github.ducknowledges.containers;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.LinkedList;
 
 public class CustomerReverseOrder {
 
-    private final Deque<Customer> customers = new LinkedList<>();
+    private final Deque<Customer> customers = new ArrayDeque<>();
 
     public void add(Customer customer) {
-        customers.addFirst(customer);
+        customers.push(customer);
     }
 
     public Customer take() {
-        return customers.pollFirst();
+        return customers.pop();
     }
 }
